91416c738b93efb4658d4e91828bc655930041d8
nathandaven <nathandaven@gmail.com>
2021-07-09 00:15:05 -0400

    More fixes for photobook

--- src/Components/GooglePhotoList.js
+++ src/Components/GooglePhotoList.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
+import { motion } from "framer-motion";
 
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -18,15 +19,17 @@ function GooglePhotosList(props) {
       );
       if (!shouldCancel && response.data && response.data.length > 0) {
         setImages(response.data);
-        setLoadedImages(response.data.splice(0, 5));
+        setLoadedImages(response.data.splice(0, 4));
       }
     };
     call();
-    return () => (shouldCancel = true);
+    return () => {
+      shouldCancel = true;
+    };
   }, [props.galleryID]);
 
   const fetchMoreData = () => {
-    setLoadedImages(loadedImages.concat(images.splice(0, 5)));
+    setLoadedImages(loadedImages.concat(images.splice(0, 1)));
   };
 
   if (props.galleryID) {
@@ -46,9 +49,30 @@ function GooglePhotosList(props) {
           {console.log(images)}
           {console.log(loadedImages)}
           {loadedImages.map((src, index) => (
-            <div className="flex text-center justify-center h-full" key={index}>
-              <img className="py-4 w-full" src={`${src}=w1200`} alt="" />
-            </div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: {
+                  scale: 0.8,
+                  opacity: 0,
+                },
+                visible: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.2,
+                  },
+                },
+              }}
+            >
+              <div
+                className="flex flex-col text-center justify-center h-full"
+                key={index}
+              >
+                <img className="py-4 w-full" src={`${src}=w1200`} alt="" />
+              </div>
+            </motion.div>
           ))}
         </InfiniteScroll>
       </div>
--- src/Screens/PhotobookScreen.js
+++ src/Screens/PhotobookScreen.js
@@ -5,11 +5,10 @@ import Page from "../Components/Page";
 import GooglePhotoList from "../Components/GooglePhotoList";
 import { Link } from "react-router-dom";
 import Footer from "../Pages/Footer";
+import { motion } from "framer-motion";
 
 function PhotobookScreen() {
-  const [currentGallery, setGallery] = React.useState(null);
-
-  React.useEffect(() => {}, [{ currentGallery }]);
+  const [currentGallery, setGallery] = React.useState("XaVbN7HVYqgGagk66");
 
   return (
     <>
@@ -17,42 +16,60 @@ function PhotobookScreen() {
         <PhotobookHeader />
         <Page>
           <div className="my-20"></div>
-          <div className="y-20 w-3/4 text-left">
-            <h1 className="text-4xl pb-10">
-              Welcome to my <b>Photobook.</b>
-            </h1>
-            <h4 className="py-2 text-2xl">Select an album:</h4>
-            <div className="flex justify-between py-10">
-              <button
-                onClick={() => setGallery("XaVbN7HVYqgGagk66")}
-                className="text-left transform hover:scale-110"
-              >
-                Random Selections
-              </button>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.8,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.2,
+                },
+              },
+            }}
+          >
+            <div className="my-20 w-full md:w-3/4 text-left">
+              <h1 className="text-4xl pb-10">
+                Welcome to my <b>Photobook.</b>
+              </h1>
+              <h4 className="py-2 text-2xl">Select an album:</h4>
+              <div className="flex flex-col sm:flex-row justify-between py-10">
+                <button
+                  onClick={() => setGallery("XaVbN7HVYqgGagk66")}
+                  className="text-left transform focus:scale-105"
+                >
+                  Random Selections
+                </button>
 
-              <button
-                onClick={() => setGallery("jA3ZRcm7KYxdYJe96")}
-                className="text-left transform hover:scale-110"
-              >
-                December 2020
-              </button>
+                <button
+                  onClick={() => setGallery("jA3ZRcm7KYxdYJe96")}
+                  className="text-left transform focus:scale-105"
+                >
+                  December 2020
+                </button>
 
-              <button
-                onClick={() => setGallery("")}
-                className="text-left transform hover:scale-110"
-              ></button>
+                <button
+                  onClick={() => setGallery("")}
+                  className="text-left transform focus:scale-110"
+                ></button>
 
-              <button
-                onClick={() => setGallery("")}
-                className="text-left transform hover:scale-110"
-              ></button>
+                <button
+                  onClick={() => setGallery("")}
+                  className="text-left transform hover:scale-110"
+                ></button>
 
-              <button
-                onClick={() => setGallery("")}
-                className="text-left transform hover:scale-110"
-              ></button>
+                <button
+                  onClick={() => setGallery("")}
+                  className="text-left transform hover:scale-110"
+                ></button>
+              </div>
             </div>
-          </div>
+          </motion.div>
           <div className=""></div>
           <GooglePhotoList galleryID={currentGallery} />{" "}
           {/* XaVbN7HVYqgGagk66 */}
