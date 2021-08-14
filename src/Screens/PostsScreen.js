b0f7536fb1a825679e2ceb1f6cfb7117ea7a3ed2
nathandaven <nathandaven@gmail.com>
2021-08-14 11:29:24 -0400

    Adjust post screen colors

--- src/Screens/PostsScreen.js
+++ src/Screens/PostsScreen.js
@@ -81,7 +81,7 @@ function PostsScreen() {
   return (
     <>
       <main
-        className="w-full ml-auto text-primarygrey dark:text-codewhite"
+        className="w-full ml-auto bg-codewhitedark dark:bg-primarygrey text-primarygrey dark:text-codewhite"
         role="main"
       >
         <Header />
@@ -111,60 +111,62 @@ function PostsScreen() {
               <h4 className="py-2 text-2xl">Pick my brain for a bit:</h4>
             </div>
           </motion.div>
-          {posts.map((post) => (
-            <motion.div
-              className="w-full"
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 0.8,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.2,
+          <div className="mb-10">
+            {posts.map((post) => (
+              <motion.div
+                className="w-full"
+                initial="hidden"
+                animate="visible"
+                variants={{
+                  hidden: {
+                    scale: 0.8,
+                    opacity: 0,
                   },
-                },
-              }}
-            >
-              <Card variant="LIGHT" key={post.fields.title.toString()}>
-                <div className="flex flex-col md:flex-row justify-between py-6">
-                  <div className="flex flex-col md:flex-row justify-start text-left font-sans">
-                    <img
-                      className="w-full md:w-48 h-full rounded-lg shadow-lg "
-                      src={post.fields.logo.fields.file.url}
-                      alt="Post logo"
-                    />
-                    <div class="md:pl-6 pt-6 md:pt-0">
-                      <h1 className="text-2xl">
-                        <b>{post.fields.title}</b>
-                      </h1>
-                      <h2 className="text-xl">
-                        <span>{formatDate(post.sys.updatedAt)}</span>
-                        {/* <span>{post.fields.date}</span> */}
-                      </h2>
-                      <p className="pt-4">{post.fields.description}</p>
+                  visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                      delay: 0.2,
+                    },
+                  },
+                }}
+              >
+                <Card variant="LIGHT" key={post.fields.title.toString()}>
+                  <div className="flex flex-col md:flex-row justify-between py-6">
+                    <div className="flex flex-col md:flex-row justify-start text-left font-sans">
+                      <img
+                        className="w-full md:w-48 h-full rounded-lg shadow-lg object-cover"
+                        src={post.fields.logo.fields.file.url}
+                        alt="Post logo"
+                      />
+                      <div class="md:pl-6 pt-6 md:pt-0">
+                        <h1 className="text-2xl">
+                          <b>{post.fields.title}</b>
+                        </h1>
+                        <h2 className="text-xl">
+                          <span>{formatDate(post.sys.updatedAt)}</span>
+                          {/* <span>{post.fields.date}</span> */}
+                        </h2>
+                        <p className="pt-4">{post.fields.description}</p>
+                      </div>
+                    </div>
+                    <div className="flex flex-col justify-center  pt-6 md:pt-0 md:pr-2">
+                      <Link
+                        className="w-full"
+                        to={"/posts/" + post.fields.slug}
+                        post={post}
+                        title={post.fields.title}
+                      >
+                        <button className="w-full px-6 py-2 mx-2 my-1 rounded-md bg-green-600 hover:bg-green-700  text-white  text-md font-sans drop-shadow-md">
+                          Read More >
+                        </button>
+                      </Link>
                     </div>
                   </div>
-                  <div className="flex flex-col justify-center  pt-6 md:pt-0 md:pr-2">
-                    <Link
-                      className="w-full"
-                      to={"/posts/" + post.fields.slug}
-                      post={post}
-                      title={post.fields.title}
-                    >
-                      <button className="w-full px-6 py-2 mx-2 my-1 rounded-md bg-green-600 hover:bg-green-700  text-white  text-md font-sans drop-shadow-md">
-                        Read More >
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </Card>
-            </motion.div>
-          ))}
+                </Card>
+              </motion.div>
+            ))}
+          </div>
           <div className=""></div>
         </Page>
         <Footer />
